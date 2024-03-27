1b8bd39519be7cd9525841c597ecc7e1b58e56f4
Altrrx <Altairmaglianesi@hotmail.com>
2024-03-27 00:57:47 -0300

    Garbageware [DANGER!!]

--- CRT_GRBG.bat
+++ CRT_GRBG.bat
@@ -1,6 +1,18 @@
 @echo off
-:create
-"%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
-echo create more garbage?
+echo off
+title GARBAGEWARE AND EPILEPSY WARNING 
+echo This is a garbageware, this can cause oversaturated system issues and is dangerous to play with, are you sure you want to use this?
+echo This can also provoke EPILEPSY!!!
 pause
-goto :create
+echo Remember. YOU ARE PLAYING WITH FIRE (dont commit arson too!) THIS IS SERIOUS.
+echo AND YOU ARE PLAYING WITH YOUR HEALTH TOO. PLEASE DONT RUN THIS IF YOU HAVE EPILEPSY OR SEIZURES!!!!
+pause
+:create1
+color f0
+"%random%" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+goto :create2
+
+:create2
+color 0f
+"%random%" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+goto :create1
