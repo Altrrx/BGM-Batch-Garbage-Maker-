bef702062f1edc22986fbf0e853e8be1ef7c5591
Altrrx <Altairmaglianesi@hotmail.com>
2024-03-27 00:43:34 -0300

    Cloning Garbage

--- CRT_GRBG.bat
+++ CRT_GRBG.bat
@@ -1,6 +1,283 @@
 @echo off
+title Branch 1
 :create
-"%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
-echo create more garbage?
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"@echo off
+title Branch %random%
+:create
+"" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
+pause
+goto :create" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage clones?
 pause
 goto :create
