6f50b7ecbcf2c05f8f30209e3c3bcbdf56ed87f9
Altrrx <Altairmaglianesi@hotmail.com>
2024-03-27 00:32:35 -0300

    Main Project + Starter Trash

---
+++ CRT_GRBG.bat
@@ -0,0 +1,6 @@
+@echo off
+:create
+"%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%" > C:\Users\Public\Desktop\Garbage Folder\%random%_garbage.txt
+echo create more garbage?
+pause
+goto :create
---
+++ read me or windows buly yu.md
@@ -0,0 +1,30 @@
+2027/3/2024 | 00:00  AM - 00:26 AM
+----------------------------
+Project by ALTRRX
+
+
+wazzup lulz i made a garbaje maekr!
+i use batch becausss lua hard and i use screartch! and barthc!11
+i develop bobox gaem
+but ye
+this is a garbej meker
+it uses %random% to maek numbre!11
+plz run in admin or windows bully and dont let yu!11
+admin makes window beg for admin!1111 like in bobox!
+
+if u no use admin:
+loser im a mod and ur a user!1111 WINDOWS WIN!11 loseeeer!
+
+if u use admin:
+pray to our god hes a admin you win please give me admin plzplzplz
+
+and if ur on admin but windows confused u forgot to put garbajje folder in the desktop!111 you dumbbe!111
+
+if no garbeje folder in desktop:
+were is the garbaje folder111!! im gonna keep this garbaje for miself!111
+
+if garbaje foleder in dektop
+ther u go ser111 i therew trash in the garbaje folder!
+
+bye bye sily men
+
